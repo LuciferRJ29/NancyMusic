12acb6c0c2859cd00c797c59b979745c5308f142
̶L͛ᴜ̶ᴄ̶ɪ̶F̶̶͛ᴇ̶ʀ̶༨✦ ܢ <112555364+LuciferRJ29@users.noreply.github.com>
2022-09-05 16:40:43 +0530

    Create __init__.py

---
+++ Nancy/__init__.py
@@ -0,0 +1,42 @@
+
+from BOBBY.core.bot import BOBBYBot
+from BOBBY.core.dir import dirr
+from BOBBY.core.git import git
+from BOBBY.core.userbot import Userbot
+from BOBBY.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = BOBBYBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
