af5d5dcc4e8838c88f2d1335b31d6ddd1a64a535
̶L͛ᴜ̶ᴄ̶ɪ̶F̶̶͛ᴇ̶ʀ̶༨✦ ܢ <112555364+LuciferRJ29@users.noreply.github.com>
2022-09-06 16:08:45 +0530

    Update pause.py

--- Nancy/plugins/Robot/pause.py
+++ Nancy/plugins/Robot/pause.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from BOBBY import app
-from BOBBY.core.call import Bobby
-from BOBBY.utils.database import is_music_playing, music_off
-from BOBBY.utils.decorators import AdminRightsCheck
+from Nancy import app
+from Nancy.core.call import Nancy
+from Nancy.utils.database import is_music_playing, music_off
+from Nancy.utils.decorators import AdminRightsCheck
 
 # Commands
 PAUSE_COMMAND = get_command("PAUSE_COMMAND")
@@ -25,7 +25,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Bobby.pause_stream(chat_id)
+    await Nancy.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention)
     )
