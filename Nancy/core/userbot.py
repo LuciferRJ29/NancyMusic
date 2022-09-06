11d02c2c571badab53158257fa0b2ecd3116e61e
̶L͛ᴜ̶ᴄ̶ɪ̶F̶̶͛ᴇ̶ʀ̶༨✦ ܢ <112555364+LuciferRJ29@users.noreply.github.com>
2022-09-06 16:37:26 +0530

    Update userbot.py

--- Nancy/core/userbot.py
+++ Nancy/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("comrade_robotz")
-                await self.one.join_chat("comrade_bots")
+                await self.one.join_chat("DevilsHaveliMF")
+                await self.one.join_chat("DevilsServerMF")
             except:
                 pass
             assistants.append(1)
@@ -78,8 +78,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("comrade_robotz")
-                await self.two.join_chat("comrade_bots")
+                await self.two.join_chat("DevilsHaveliMF")
+                await self.two.join_chat("DevilsServerMF")
             except:
                 pass
             assistants.append(2)
@@ -108,8 +108,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("comrade_robotz")
-                await self.three.join_chat("comrade_bots")
+                await self.three.join_chat("DevilsHaveliMF")
+                await self.three.join_chat("DevilsServerMF")
             except:
                 pass
             assistants.append(3)
@@ -138,8 +138,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("comrade_robotz")
-                await self.four.join_chat("comrade_bots")
+                await self.four.join_chat("DevilsHaveliMF")
+                await self.four.join_chat("DevilsServerMF")
             except:
                 pass
             assistants.append(4)
@@ -168,8 +168,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("comrade_robotz")
-                await self.five.join_chat("comrade_bots")
+                await self.five.join_chat("DevilsHaveliMF")
+                await self.five.join_chat("DevilsServerMF")
             except:
                 pass
             assistants.append(5)
