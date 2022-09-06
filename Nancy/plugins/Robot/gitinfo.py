dc318b57c29c803f82f45339f69e0055834c0c5f
̶L͛ᴜ̶ᴄ̶ɪ̶F̶̶͛ᴇ̶ʀ̶༨✦ ܢ <112555364+LuciferRJ29@users.noreply.github.com>
2022-09-06 15:57:48 +0530

    Update gitinfo.py

--- Nancy/plugins/Robot/gitinfo.py
+++ Nancy/plugins/Robot/gitinfo.py
@@ -1,12 +1,12 @@
 import aiohttp
 from pyrogram import filters
-from BOBBY import app
+from Nancy import app
 
 
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git love-u-bobby")
+        await message.reply_text("/git LuciferRJ29")
         return
     username = message.text.split(None, 1)[1]
     URL = f'https://api.github.com/users/{username}'
