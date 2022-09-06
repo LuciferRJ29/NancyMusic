2e1dcdb2d99216b94ad747c95e4aff9552055160
̶L͛ᴜ̶ᴄ̶ɪ̶F̶̶͛ᴇ̶ʀ̶༨✦ ܢ <112555364+LuciferRJ29@users.noreply.github.com>
2022-09-06 16:38:37 +0530

    Update mongo.py

--- Nancy/core/mongo.py
+++ Nancy/core/mongo.py
@@ -11,10 +11,10 @@ TEMP_MONGODB = "mongodb+srv://anonxmusic:anonxmusic@anonx.9v4i8.mongodb.net/?ret
 
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "Your bot will work on @love_you_bobby mongo database..."
+        "Your bot will work on @Lucifer_was_bot mongo database..."
     )
     temp_client = Client(
-        "BOBBY",
+        "Nancy",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
@@ -30,5 +30,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Bobby
-    pymongodb = _mongo_sync_.Bobby
+    mongodb = _mongo_async_.Nancy
+    pymongodb = _mongo_sync_.Nancy
