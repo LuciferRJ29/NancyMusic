1a0fa6c6dd24bf8c5bd52a06e74567978e4abf53
̶L͛ᴜ̶ᴄ̶ɪ̶F̶̶͛ᴇ̶ʀ̶༨✦ ܢ <112555364+LuciferRJ29@users.noreply.github.com>
2022-09-06 16:42:07 +0530

    Update bot.py

--- Nancy/core/bot.py
+++ Nancy/core/bot.py
@@ -8,11 +8,11 @@ from ..logging import LOGGER
 
 
 
-class BOBBYBot(Client):
+class NancyBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "BobbyMusic",
+            "NancyMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
